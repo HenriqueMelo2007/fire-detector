411c87b73913dca253bbfecbc6668c4e8150af52
HenriqueMelo2007 <henriquemelo0983@gmail.com>
2025-04-08 15:05:40 -0300

    it adds a temporary home page

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,5 +1,10 @@
+import Link from "next/link";
+
 export default function Home() {
   return (
-    <div className="text-red-500">New code</div>
+    <div className="w-screen h-screen flex justify-center items-center flex-col">
+      <h1>Here, there will be a home page</h1>
+      <Link className="bg-red-500 p-5 rounded-2xl text-white" href="/dashboard">Dashboard</Link>
+    </div>
   );
 }
