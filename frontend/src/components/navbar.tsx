27e50494c772a7aff88fd3ac034b095c1c0b6ac3
HenriqueMelo2007 <henriquemelo0983@gmail.com>
2025-04-14 13:05:44 -0300

    style: it adds margin bottom

--- frontend/src/components/navbar.tsx
+++ frontend/src/components/navbar.tsx
@@ -2,7 +2,7 @@ import Btn from './btn'
 
 export default function NavBar() {
   return (
-    <nav className="flex justify-between py-4 px-12 font-bold text-center">
+    <nav className="flex justify-between py-4 px-12 font-bold text-center mb-5">
       <h1 className="text-2xl">Fire Detector - Thermophilic</h1>
       <div className="flex gap-10">
         <Btn href="/signup" text="Sign up" bg="bg-gray-400"></Btn>
