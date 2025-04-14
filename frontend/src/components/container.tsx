8aa3ad946e1499173156a320d5adbc0266c763ff
HenriqueMelo2007 <henriquemelo0983@gmail.com>
2025-04-14 13:07:19 -0300

    style: it adds margin bottom

--- frontend/src/components/container.tsx
+++ frontend/src/components/container.tsx
@@ -6,7 +6,7 @@ interface containerprops {
 
 export default function Container({ children }: containerprops) {
   return (
-    <div className="h-screen w-screen flex justify-between items-center px-12 gap">
+    <div className="min-h-screen w-screen flex justify-between items-center px-12 mb-5">
       {children}
     </div>
   )
