8eb7a5a61351873433e855433f15fce54dcb1f20
HenriqueMelo2007 <henriquemelo0983@gmail.com>
2025-04-14 11:34:27 -0300

    feat: it adds bg to btn component

--- frontend/src/components/btn.tsx
+++ frontend/src/components/btn.tsx
@@ -3,8 +3,9 @@ import Link from "next/link";
 interface btnprops {
   text: string,
   href: string,
+  bg: string,
 }
 
-export default function Btn({ text, href }: btnprops) {
-  return <Link href={href}>{text}</Link>
+export default function Btn({ text, href, bg }: btnprops) {
+  return <Link href={href} className={`${bg} text-center text-black rounded-xl py-1.5 px-5`}>{text}</Link>
 }
