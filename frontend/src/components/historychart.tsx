54f549dd0b1bdff502c89434600edcbd36ce2e03
HenriqueMelo2007 <henriquemelo0983@gmail.com>
2025-04-14 13:06:47 -0300

    feat: it creates the component

--- frontend/src/components/historychart.tsx
+++ frontend/src/components/historychart.tsx
@@ -1,3 +1,21 @@
 export default function HistoryChart() {
-  return <div>HistoryChart</div>
+  return (
+    <div className="bg-gray-800 flex items-center justify-center mt-10 text-gray-200">
+      <div className="bg-gray-800 shadow-md rounded-lg p-6">
+        <h1 className="text-2xl font-bold mb-4">History of Presence of Smoke or Flammable Gases</h1>
+        <svg
+          viewBox="0 0 300 150"
+          className="w-full h-64"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <polyline
+            fill="none"
+            stroke="#EF4444"
+            strokeWidth="1"
+            points="0,100 33,70 66,90 100,50 133,80 166,30 200,70 233,20 266,60 300,40"
+          />
+        </svg>
+      </div>
+    </div>
+  )
 }
