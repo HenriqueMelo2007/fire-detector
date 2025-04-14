b451bf64f009fb5243cc0f90b95c8fa78f574c4d
HenriqueMelo2007 <henriquemelo0983@gmail.com>
2025-04-14 13:03:52 -0300

    feat: it adds a better description about the platform

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -11,9 +11,18 @@ export default function Home() {
         <div className="w-1/2 h-full flex flex-col justify-center">
           <h2 className="font-bold text-4xl mb-4">Thermophilic</h2>
           <h2 className="font-bold text-4xl mb-8">Intelligent fire detector</h2>
+          <p className="text-gray-400 mb-4">
+            This project is a web platform that allows users to monitor and
+            visualize data from an intelligent fire detection system. After
+            registering, users can link their personal fire detection devices to
+            the platform. The system continuously collects environmental data
+            such as light intensity, humidity, temperature, and the presence of
+            smoke or flammable gases to determine if there is a potential fire.
+          </p>
           <p className="text-gray-400">
-            Fire detection system that integrates physical sensors (ESP32), a
-            Machine Learning model and a real-time web interface.
+            The platform then displays detailed current sensor readings,
+            historical graphs of the last ten measurements for each parameter,
+            and alerts users to the most recent fire detection event.
           </p>
         </div>
         <div className="w-1/2 h-full flex flex-col items-center justify-center text-center">
