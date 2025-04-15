030a679bb9dd6f297009c1eae8d26999b8b99df5
HenriqueMelo2007 <henriquemelo0983@gmail.com>
2025-04-15 17:14:45 -0300

    style: ti changes btn component style

--- frontend/src/components/navbar.tsx
+++ frontend/src/components/navbar.tsx
@@ -5,8 +5,8 @@ export default function NavBar() {
     <nav className="flex justify-between py-4 px-12 font-bold text-center mb-5">
       <h1 className="text-2xl">Fire Detector - Thermophilic</h1>
       <div className="flex gap-10">
-        <Btn href="/signup" text="Sign up" bg="bg-gray-400"></Btn>
-        <Btn href="/login" text="Login" bg="bg-gray-400"></Btn>
+        <Btn href="/signup" text="Sign up"></Btn>
+        <Btn href="/login" text="Login"></Btn>
       </div>
     </nav>
   )
