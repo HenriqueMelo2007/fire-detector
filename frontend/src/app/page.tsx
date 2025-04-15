707af24e8a71c9a4eda0ede947fe34debf6a628e
HenriqueMelo2007 <henriquemelo0983@gmail.com>
2025-04-15 16:56:42 -0300

    feat: it adds red smoke efffect below github button

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,3 +1,5 @@
+import Image from 'next/image'
+
 import NavBar from '@/components/navbar'
 import MeasurementImage from '@/components/measurementimage'
 import GithubBtn from '@/components/githubbtn'
@@ -9,7 +11,7 @@ export default function Home() {
     <div className="flex flex-col">
       <NavBar></NavBar>
       <div className="min-h-screen w-screen flex justify-between items-center px-12 mb-5">
-        <div className="w-1/2 h-full flex flex-col justify-center gap-8">
+        <div className="w-1/2 h-screen flex flex-col gap-8 ">
           <div>
             <h2 className="font-bold text-4xl mb-4">Thermophilic</h2>
             <h2 className="font-bold text-4xl mb-8">
@@ -69,6 +71,9 @@ export default function Home() {
           </div>
           <div className="flex justify-center items-center">
             <GithubBtn></GithubBtn>
+          </div>
+          <div className='flex justify-center items-center opacity-70'>
+            <Image src="/bottom-image.png" alt="Smoke effect image" width={800} height={242}></Image>
           </div>
         </div>
         <div className="w-1/2 h-full flex flex-col items-center justify-center text-center px-10">
